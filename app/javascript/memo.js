172197c56df85106a0c1ac0fae58627534dcc79f
ohkuma90 <s.ohkuma001@gmail.com>
2023-08-23 20:22:50 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,20 @@
+const buildHTML = (XHR) => {
+  // itemにレスポンスの内容post（コントローラーから送られたやつ）を代入、
+  const item = XHR.response.post;
+  // htmlに反映させる情報を代入
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  // 出来たhtmlをリターンする
+  return html;
+};
+
 function post (){
   // id="form"の取得
   const form = document.getElementById("form");
@@ -14,6 +31,21 @@ function post (){
     XHR.responseType = "json";
     // formDataを持ってリクエスト送信
     XHR.send(formData);
+    // リクエストの送信が成功した時の処理を記述
+    XHR.onload = ()=> {
+      // レスポンスに何かしら問題があった時の条件分岐
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      // list要素の取得
+      const list = document.getElementById("list");
+      // フォームのテキストフィールドを取得
+      const formText = document.getElementById("content");
+      // buildHTMLを呼び出しhtml作り、挿入箇所をlistの直後に指定して挿入
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
